855d4f369d7c326b291ddff21d8d26cebfb19bfc
Kedi Krasniqi <kedikrasniqi@hotmail.com>
2022-06-29 08:38:49 +0200

    done

--- my-react-portofolio/src/components/about/About.jsx
+++ my-react-portofolio/src/components/about/About.jsx
@@ -13,26 +13,26 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <div className="about__me-img">
+          <div className="about__me-image">
             <img src={ME} alt="About Image" />
           </div>
         </div>
 
         <div className="about__content">
           <div className="about__cards">
-            <article className="About__card">
+            <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
 
-            <article className="About__card">
+            <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>300+ WorldWide</small>
             </article>
 
-            <article className="About__card">
+            <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>80+ completed prjoects</small>
