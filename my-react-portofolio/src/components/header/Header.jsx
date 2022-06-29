454ba519c8480175d9cf41b36a175a84e8e80817
Kedi Krasniqi <kedikrasniqi@hotmail.com>
2022-06-29 08:58:36 +0200

    done

--- my-react-portofolio/src/components/header/Header.jsx
+++ my-react-portofolio/src/components/header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
           <img src={ME} alt="" />
         </div>
 
-        <a href="#contacy" className="scroll__down">
+        <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
       </div>
